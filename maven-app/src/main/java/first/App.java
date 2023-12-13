5af6852b051d88990ad482fd58066e4d6ef168e5
habibariad99 <habiba.riad123@gmail.com>
2023-12-13 22:59:48 +0200

    docker file

--- maven-app/src/main/java/first/App.java
+++ maven-app/src/main/java/first/App.java
@@ -4,10 +4,11 @@ package first;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Entry point of your program
+        // Add your code here
+
+        System.out.println("hello world") ;
     }
 }
